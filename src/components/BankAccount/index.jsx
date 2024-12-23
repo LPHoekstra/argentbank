ed20a248904a6bf61c11a6f0c39d155b3d007bb6
MalBourbie <MalBourbie@gmail.com>
2024-12-23 20:17:13 +0100

    css change for a better responsive. rename the mixin "smallScreen" to "largeScreen" and change the media queries value.

--- src/components/BankAccount/index.jsx
+++ src/components/BankAccount/index.jsx
@@ -7,7 +7,7 @@ function BankAccount({ amount, amountDescription, title }) {
             <div className={m.account__contentWrapper}>
                 <h3 className={m.account__title}>{title}</h3>
                 <p className={m.account__amount}>{amount}</p>
-                <p className={m.account__amount__description}>{amountDescription}</p>
+                <p>{amountDescription}</p>
             </div>
             <div className={`${m.account__contentWrapper} cta`}>
                 <button className={`button ${m.account__transactionButton}`}>View transactions</button>
--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -10,13 +10,13 @@ function Header() {
         <nav className={m.mainNav}>
             <Link className={m.mainNav__logo} to="/">
                 <img
-                    className={m.mainNav__img}
+                    className={m.mainNav__logoImg}
                     src={argentBankLogo}
                     alt="Argent Bank Logo"
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
-            <div>
+            <div className={m.mainNav__linkContainers}>
                 {!isConnected || <Link className={m.mainNav__item} to="/profile">Profile</Link>}
                 <Link className={m.mainNav__item} to={isConnected ? "/logout" : "/login"}>
                     <i className="fa fa-user-circle"></i>
